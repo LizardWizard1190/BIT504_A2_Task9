093605821d555c35bc15db954690fd806b3b9bb2
LizardWizard1190 <liz.cavenagh@gmail.com>
2026-04-27 13:35:34 +1200

    Initial Commit - basic java setup

---
+++ src/task9/Main.java
@@ -0,0 +1,11 @@
+package task9;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Task 9 GitHub project");
+		System.out.println("First version of the program");
+
+	}
+
+}
